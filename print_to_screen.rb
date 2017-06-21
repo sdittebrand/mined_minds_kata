6a9854ce31a3ddbde77ddc54442e0ba6cbc2d62a
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-21 15:33:08 -0400

    replaced odds with 'odd

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -2,8 +2,10 @@ number = 1
 100.times do
     if (number % 2 == 0)
         puts "even"
-    else
-        puts number
+    elsif 
+        (number % 2 == 1)
+        puts "odd"
+        
     end
     number = number + 1
 end
